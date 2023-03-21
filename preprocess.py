1b8e5d02465509c7662e3b7155c8e0ed80f051b4
Mre-Dennis-Kinuthia <Denniskinuthia1105@gmail.com>
2023-03-21 16:59:09 +0300

    Preprocessor script added

---
+++ preprocess.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from PIL import Image
+import os
+
+# Define the path to the dataset directory
+dataset_path = '/home/nansi/limasphere-engines/the-eye-of-limasphere/datasets'
+
+# Define a list of labels fr each class of image
+labels = ['healthy', 'pest', 'disease']
+
+# load the images and labels for each class of image
+data = []
+for label in labels:
+    label_path = os.path.join(dataset_path, label)
+    for filename in os.listdir(label_path):
+        img_path = os.path.join(label_path, filename)
+        img = Image.open(img_path)
+        img = img.resize((224, 224)) # Resize the image to 224x224 pixels
+        data.append((img, label))
+        
+# split the data into training, validation, and testing sets
+training_data = data[:int(0.7*len(data))]
+validation_data = data[int(0.7*len(data)):int(0.85*len(data))]
+test_data = data[int(0.85*len(data)):]
